46a7fd383d918534f9d0f074dd1b1318380f31f0
Ilya <nechaev.illia@gmail.com>
2019-04-06 02:39:47 +0300

    Сборка ДЗ

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let money = prompt("Ваш бюджет на месяц?");
+let money = +prompt("Ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let ans1 = prompt ("Введите обязательную статью расходов в этом месяце");
-let ans2 = prompt ("Во сколько обойдется?");
-let ans3 = prompt ("Введите обязательную статью расходов в этом месяце");
-let ans4 = prompt ("Во сколько обойдется?");
+
 
 var appData = {
     budget: money,
@@ -15,7 +12,46 @@ var appData = {
     savings: false
 };
 
-appData.expences.ans1 = ans2;
-appData.expences.ans3 = ans4;
+// for (let i=0; i<2; i++){
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце");
+//     let b = prompt ("Во сколько обойдется?");    
 
-alert("Ваш бюджет на день: " + appData.budget/30);
+//     if( (typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) && a != "" && b != "" & a.length < 50 ){
+//         console.log("done");
+//         appData.expences[a] = b;
+//     } else {
+//         console.log("error");
+//         i--;
+//     }       
+    
+// }
+
+// let j = 0;
+// while (j < 2){
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце");
+//     let b = prompt ("Во сколько обойдется?");  
+//     j++;
+
+//     if( (typeof(a) === "string") && (typeof(a) != null) && (typeof(b) != null) && a != "" && b != "" & a.length < 50 ){
+//         console.log("done");
+//         appData.expences[a] = b;
+//     } else {
+//         console.log("error");
+//         j--;
+//     } 
+// }
+
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay >=2000){
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Ошибка");
+}
